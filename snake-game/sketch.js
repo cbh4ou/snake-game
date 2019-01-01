56c3ab8a07bb77562e81d2b31492f45abbc29cee
Connor Brent Hatcher <cbh4ou@gmail.com>
2019-01-01 00:11:11 -0600

    First Version, will add highscores with Java/mysql

--- snake-game/sketch.js
+++ snake-game/sketch.js
@@ -1,3 +1,5 @@
+var s;
+
 function setup() {
   createCanvas(displayWidth, displayHeight);
   colorMode(RGB);
@@ -17,12 +19,6 @@ function touchMoved() {
   ellipse(touchX, touchY, 40, 40);
 }
 
-function touchEnded() {
-  var r = random(255);
-  var g = random(255);
-  var b = random(255);
-  background(r, g, b);
-}
 
 
 
